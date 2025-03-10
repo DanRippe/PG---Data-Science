c6b4f981fe2ee1847000d074d4359747e491b183
Danilo Cardoso <danilo.mecatronica2015@gmail.com>
2025-03-10 18:42:59 -0300

    first

---
+++ Python/1-Primeiros_Passos/teste.py
@@ -0,0 +1,20 @@
+a = 10
+# valor numerico int
+
+b = "Descomplica"
+# string
+
+c = 40.5
+# valor numerico double (float)
+
+d = "30"
+# string com sequencia numerica
+
+MAIORIDADE = 18
+# representacao de const
+# no python nativamente nao tem const, o valor pode ser alterado
+# comentar varias linhas - ctrl + ;
+
+nomeDoFuncionario = "Danilo"
+# Camel Case para criacao de variaveis com mais de uma palavra
+
