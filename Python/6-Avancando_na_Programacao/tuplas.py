81f299a8b9c6a0dc1d12d8cb8180a0a723a56825
Danilo Cardoso <danilo.mecatronica2015@gmail.com>
2026-01-22 11:20:30 -0300

    new

---
+++ Python/6-Avancando_na_Programacao/tuplas.py
@@ -0,0 +1,8 @@
+# As tuplas são, essencialmente similares as listas, a diferença é que elas são imutáveis
+
+tupla = (1,2,3,4,5,6,7,8,9,0)
+
+if 8 in tupla:
+    print("Existe")
+else:
+    print("Não existe")
