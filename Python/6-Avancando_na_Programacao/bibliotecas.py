91f8ffef2d9f640156e1ad8613dc9bdf0b129ad0
Danilo Cardoso <danilo.mecatronica2015@gmail.com>
2026-01-22 11:32:42 -0300

    new

---
+++ Python/6-Avancando_na_Programacao/bibliotecas.py
@@ -0,0 +1,5 @@
+# As bibliotecas também funconam de forma similar às listas, mas ao invés de terem um índice numérico sequencial "oculto", utilizam um índice textual
+# e uma valor atribuído para cada índice
+
+lista = {"nome":"Danilo", "sobrenome":"Cardoso"}
+print(lista["nome"], lista["sobrenome"])
