e1efb5b61383beb1e39e9074de3f7b68536ceeee
Danilo Cardoso <danilo.mecatronica2015@gmail.com>
2026-01-24 09:31:16 -0300

    new

--- Python/7-Programacao_Orientada_a_Objetos/Classes_Metodos_Atributos.py
+++ Python/7-Programacao_Orientada_a_Objetos/Classes_Metodos_Atributos.py
@@ -1,10 +1,12 @@
 from Pessoa import Pessoa
 
-pessoa = Pessoa()
+#pessoa = Pessoa()
 
 #print(pessoa.nome)
 #print(pessoa.idade)
 
-pessoa.nome = "Lilian"
-pessoa.idade = 34
-pessoa.exibir()
+#pessoa.nome = "Lilian"
+#pessoa.idade = 34
+#pessoa.exibir()
+
+pessoa = Pessoa("Lilian", 35)
--- Python/7-Programacao_Orientada_a_Objetos/Pessoa.py
+++ Python/7-Programacao_Orientada_a_Objetos/Pessoa.py
@@ -11,3 +11,19 @@ class Pessoa:
     def exibir(self):       #self indica que será utilizado um valor de dentro da própria classe e não um valor externo
         print(self.nome)
         print(self.idade)
+
+    #método construtor
+    #def __init__(self, nome, idade):
+    #    self.nome = nome
+    #    self.idade = idade
+    #    print(self.nome)
+    #    print(self.idade)
+
+    #GETTERS E SETTERS
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
